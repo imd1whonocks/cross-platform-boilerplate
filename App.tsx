9ea2d9902bcd5f7cf635bd6ddde2a26722f4f1d4
imd1whonocks <shashank.2410@gmail.com>
2021-04-02 21:17:08 +0530

    chore: UI chnages

--- App.tsx
+++ App.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import {
+  Image,
   SafeAreaView,
   ScrollView,
   StatusBar,
@@ -9,100 +10,59 @@ import {
   View,
 } from 'react-native';
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
-
-type SectionProps = {
-  title: string,
-  children?: React.ReactNode,
-}
-
-const Section = ({children, title}: SectionProps): JSX.Element => {
-  const isDarkMode = useColorScheme() === 'dark';
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
-};
-
+import { Colors } from 'react-native/Libraries/NewAppScreen';
 const App: () => React.ReactNode = () => {
   const isDarkMode = useColorScheme() === 'dark';
 
   const backgroundStyle = {
     backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
   };
-
   return (
     <SafeAreaView style={backgroundStyle}>
       <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
       <ScrollView
         contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.js</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
+        style={backgroundStyle}
+        contentContainerStyle={styles.root}
+      >
+        <View 
+          style={styles.container}
+        >
+          <Image
+            style={styles.logo}
+            source={require('./assets/img/reactnative.png')}
+          />
+          <View style={styles.textBox}>
+            <Text style={styles.text}>A boilerplate for developing React Native cross-platform apps that works on iOS, Android and web.</Text>
+          </View>
         </View>
       </ScrollView>
     </SafeAreaView>
-  );
+  )
 };
 
 const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
+  root: {
+    height: '100%',
   },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
+  container: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    flex: 1,
   },
-  sectionDescription: {
-    marginTop: 8,
+  logo: {
+    width: 200,
+    height: 200,
+    marginBottom: 30,
+  },
+  textBox: {
+    width: '90%'
+  },
+  text: {
+    textAlign: 'center',
     fontSize: 18,
     fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
-  },
+  }
 });
 
 export default App;
